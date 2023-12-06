e45369d84a6c014aa9e15b9fe7b08cec672328f9
Ayoub ERROUICHAQ <ayerrouichaq@gmail.com>
2023-12-06 09:25:57 +0100

    MAJ des services pour la recherche

--- services/DPE.js
+++ services/DPE.js
@@ -1,33 +1,27 @@
-const pokedex = require('../models/DPE');
+const dpeModel = require('../models/DPE');
 const debug = require('debug')('backend:services:dpe');
 
 exports.searchMaison = (Etiquette_GES, Etiquette_DPE, Code_postal, done) => {
+    let query = {};
 
-    if (Etiquette_GES != undefined) {
-        pokedex.find(Etiquette_GES).then(data => {
-            debug('filtrage par GES');
-            return done(undefined, data);
-        }).catch(err => {
-            if (err) return done(err);
-        });
-    } else {
-        if (Etiquette_DPE != undefined) {
-            pokedex.find(Etiquette_DPE).then(data => {
-                debug('filtrage par DPE');
-                return done(undefined, data);
-            }).catch(err => {
-                if (err) return done(err);
-            });
-        } else {
-            if (Code_postal != undefined) {
-                pokedex.find(Code_postal).then(data => {
-                    debug('filtrage par Code postal');
-                    return done(undefined, data);
-                }).catch(err => {
-                    if (err) return done(err);
-                });
-            }
-            return done(new Error('Pas de DPE ou GES correspondant'));
-        }
+    if (Etiquette_GES) {
+        query.Etiquette_GES = Etiquette_GES;
     }
+    if (Etiquette_DPE) {
+        query.Etiquette_DPE = Etiquette_DPE;
+    }
+    if (Code_postal) {
+        query.Code_postal = Code_postal;
+    }
+
+    if (Object.keys(query).length === 0) {
+        return done(new Error('Pas de DPE, GES ou Code postal fourni'));
+    }
+
+    dpeModel.find(query).then(data => {
+        debug('Filtrage réussi');
+        return done(undefined, data);
+    }).catch(err => {
+        if (err) return done(err);
+    });
 }
